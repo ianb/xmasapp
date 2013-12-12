9e1fe3aee36758ae0543bbf2ed8f9e5a3eeacd53
Ian Bicking <ianb@colorstudy.com>
2013-12-12 01:49:21 -0600

    fix build with appcache.  Add favorites

--- carols.js
+++ carols.js
@@ -1,3 +1,19 @@
+function charForFavorite(faved) {
+  return faved ? "\u2605" : "\u2606";
+}
+
+function getFavorite(name) {
+  return !! localStorage.getItem("favorite." + name);
+}
+
+function setFavorite(name, faved) {
+  if (faved) {
+    localStorage.setItem("favorite." + name, "true");
+  } else {
+    localStorage.removeItem("favorite." + name);
+  }
+}
+
 window.addEventListener("load", function () {
   var songs = [];
   var songsByName = {};
@@ -12,6 +28,7 @@ window.addEventListener("load", function () {
       name: name,
       el: el
     };
+    song.favorite = getFavorite(song.name);
     var control = document.createElement("div");
     control.innerHTML = "back";
     control.className = "back";
@@ -19,6 +36,20 @@ window.addEventListener("load", function () {
     control.addEventListener("click", function () {
       back();
     }, false);
+    var favorite = document.createElement("div");
+    favorite.setAttribute("data-name", song.name);
+    favorite.innerHTML = charForFavorite(song.favorite);
+    favorite.className = "favorite";
+    el.insertBefore(favorite, el.childNodes[0]);
+    favorite.addEventListener("click", function (event) {
+      var name = event.target.getAttribute("data-name");
+      var song = songsByName[name];
+      song.favorite = ! song.favorite;
+      setFavorite(song.name, song.favorite);
+      event.target.innerHTML = charForFavorite(song.favorite);
+      refreshList();
+    });
+    console.log("ok", favorite, favorite.parentNode);
     var about = el.querySelector(".about");
     if (about) {
       about.setAttribute("target", "_blank");
@@ -29,23 +60,43 @@ window.addEventListener("load", function () {
   songs.sort(function (a, b) {
     return a.name > b.name ? 1 : -1;
   });
+
   var songList = document.getElementById("song-list");
-  var page = 2;
-  songs.forEach(function (song) {
-    var div = document.createElement("div");
-    div.className = "song-select";
-    var number = document.createElement('span');
-    number.className = 'song-number';
-    number.appendChild(document.createTextNode(page));
-    div.appendChild(document.createTextNode(song.name));
-    div.addEventListener("click", function () {
-      selectSong(song);
-    }, false);
-    div.appendChild(number);
-    songList.appendChild(div);
-    page++;
-    song.el.parentNode.removeChild(song.el);
-  });
+  var favoriteList = document.getElementById("favorite-list");
+  var songsDone = false;
+
+  function refreshList() {
+    favoriteList.innerHTML = "";
+    var page = 2;
+    songs.forEach(function (song) {
+      var div = document.createElement("div");
+      div.className = "song-select";
+      var number = document.createElement('span');
+      number.className = 'song-number';
+      number.appendChild(document.createTextNode(page));
+      div.appendChild(document.createTextNode(song.name));
+      div.addEventListener("click", function () {
+        selectSong(song);
+      }, false);
+      div.appendChild(number);
+      if (! songsDone) {
+        songList.appendChild(div);
+        song.el.parentNode.removeChild(song.el);
+      }
+      page++;
+      if (song.favorite) {
+        var clone = div.cloneNode();
+        clone.addEventListener("click", function () {
+          selectSong(song);
+        });
+        favoriteList.appendChild(clone);
+      }
+    });
+    songsDone = true;
+  }
+
+  refreshList();
+
   var songsContainer = document.getElementById("songs");
   songs.forEach(function (song) {
     songsContainer.appendChild(song.el);
@@ -68,6 +119,7 @@ window.addEventListener("load", function () {
     song.el.style.display = "";
     document.getElementById("songs").classList.remove("hidden");
     location.hash = "#" + encodeURIComponent(song.name);
+    window.scrollTo(0, 0);
   }
 
   function hashChange() {
